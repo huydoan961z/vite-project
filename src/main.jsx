d8bf0fed8125117854ffb4ebcc6973562eb3b3d2
huydoan961z <144944736+huydoan961z@users.noreply.github.com>
2024-12-10 09:58:52 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import * as ReactDOM from "react-dom/client";
+import * as React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/todolist",
+    element: <App />,
+  },
+  {
+    path: "/trial",
+    element: <div>Hello trial
+
+    </div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
